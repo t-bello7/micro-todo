0f853c43e0d330447aafdad6c84f0cae5adaeb9a
t-bello7 <bello4aus@gmail.com>
2022-06-07 22:38:14 +0100

    add utility and reset classes
    center tasks class
    
    create renderElements functin in index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+let taskContainer = document.querySelector('.tasks')
+
+let task = {
+    description: "",
+    completed: "",
+    index: ""
+}
+let taskArr = [
+    {
+        description: " wash the dishes",
+        completed: "",
+        index: "1"
+    },
+    {
+        description: "go out with friends",
+        completed: "",
+        index: "2"
+    }
+]
+
+const renderElements = (arr, container) => {
+    arr.forEach( element => {
+        let task = document.createElement('div')
+        task.innerHTML = ` <input type="checkbox"> ${element.description}`
+        container.appendChild(task);
+    })
+
+    let clearButton = document.createElement('button');
+    clearButton.innerHTML='clear completed'
+    container.appendChild(clearButton)
+}
+
+renderElements(taskArr, taskContainer);
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry : './src/index.js',
+    resolve: {
+        fallback: {
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+    }
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
