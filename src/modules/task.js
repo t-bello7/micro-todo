8358045c870539f79879825e8d1edcf43391bec8
t-bello7 <bello4aus@gmail.com>
2022-06-13 13:50:23 +0100

    - add eslint configurations - add radix 10 to parseInt functions - add strict type check

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,35 +1,36 @@
 export const getTasks = () => {
-    let taskArr = []
-    if (localStorage.getItem('taskArr') != null){
-        taskArr = JSON.parse(localStorage.getItem('taskArr'))
-    }
-    return taskArr;
-}
+  let taskArr = [];
+  if (localStorage.getItem('taskArr') != null) {
+    taskArr = JSON.parse(localStorage.getItem('taskArr'));
+  }
+  return taskArr;
+};
 
-export const addTask = (task,taskArr) => {
-    taskArr.push(task)
-    localStorage.setItem('taskArr', JSON.stringify(taskArr))
-} 
+export const addTask = (task, taskArr) => {
+  taskArr.push(task);
+  localStorage.setItem('taskArr', JSON.stringify(taskArr));
+};
+
 export const removeTask = (index, taskArr) => {
-    taskArr.splice(index-1, 1)
-    taskArr.forEach(element=> {
-        if (element.index > index){
-            element.index = element.index - 1; 
-        }
-    })
-    localStorage.setItem('taskArr', JSON.stringify(taskArr))
-} 
+  taskArr.splice(index - 1, 1);
+  taskArr.forEach((element) => {
+    if (element.index > index) {
+      element.index -= 1;
+    }
+  });
+  localStorage.setItem('taskArr', JSON.stringify(taskArr));
+};
 
-export const checkTask = ( taskCheck, index, taskArr) => {
-    taskArr.forEach(element => {
-        if (element.index === parseInt(index)){
-            if (taskCheck.checked === true){
-                element.completed = true;
-                localStorage.setItem('taskArr', JSON.stringify(taskArr))
-            } else{
-                element.completed = false;
-                localStorage.setItem('taskArr', JSON.stringify(taskArr)) 
-            }
-        }
-    }) 
-}
+export const checkTask = (taskCheck, index, taskArr) => {
+  taskArr.forEach((element) => {
+    if (element.index === parseInt(index, 10)) {
+      if (taskCheck.checked === true) {
+        element.completed = true;
+        localStorage.setItem('taskArr', JSON.stringify(taskArr));
+      } else {
+        element.completed = false;
+        localStorage.setItem('taskArr', JSON.stringify(taskArr));
+      }
+    }
+  });
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,49 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry : './src/index.js',
-    resolve: {
-        fallback: {
-        "fs": false,
-        "tls": false,
-        "net": false,
-        "path": false,
-        "zlib": false,
-        "http": false,
-        "https": false,
-        "stream": false,
-        "crypto": false,
-        "util": false,
-        "buffer": false,
-        "url": false,
-        "vm": false,
-        "querystring": false,
-        "os": false,
-        "constants": false,
-        "assert": false,
-    }
+  entry: './src/index.js',
+  resolve: {
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      stream: false,
+      crypto: false,
+      util: false,
+      buffer: false,
+      url: false,
+      vm: false,
+      querystring: false,
+      os: false,
+      constants: false,
+      assert: false,
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'output.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: 'output.bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }
-        ]
-    },
-    devServer: {
-        static: './dist',
-        port: 9000
-    },
-    mode: 'development'
-}
+  },
+  devServer: {
+    static: './dist',
+    port: 9000,
+  },
+  mode: 'development',
+};
